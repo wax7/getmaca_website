8cf12fb1aacab6d6bdebf373e5ebb4b663b50d14
Dirk Wenk <wax7@bosch-smarthome.fritz.box>
2026-04-12 15:59:16 +0200

    fix(gtag): dynamic script loading + wait_for_update to fix Tag Assistant detection

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -19,14 +19,14 @@ export function Head() {
           __html: `
 window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}
 var sc=null;try{sc=JSON.parse(localStorage.getItem('maca-consent-v2'));}catch(e){}
-if(sc){gtag('consent','default',{'ad_storage':sc.ad_storage||'denied','ad_user_data':sc.ad_user_data||'denied','ad_personalization':sc.ad_personalization||'denied','analytics_storage':sc.analytics_storage||'denied'});}
-else{gtag('consent','default',{'ad_storage':'denied','ad_user_data':'denied','ad_personalization':'denied','analytics_storage':'denied','region':['AT','BE','BG','HR','CY','CZ','DK','EE','FI','FR','DE','GR','HU','IE','IT','LV','LT','LU','MT','NL','PL','PT','RO','SK','SI','ES','SE','IS','LI','NO','GB','CH']});gtag('consent','default',{'ad_storage':'denied','ad_user_data':'denied','ad_personalization':'denied','analytics_storage':'granted','region':['US-CA']});gtag('consent','default',{'ad_storage':'denied','ad_user_data':'denied','ad_personalization':'denied','analytics_storage':'denied','region':['BR']});gtag('consent','default',{'ad_storage':'granted','ad_user_data':'denied','ad_personalization':'denied','analytics_storage':'granted','region':['JP','KR']});gtag('consent','default',{'ad_storage':'denied','ad_user_data':'denied','ad_personalization':'denied','analytics_storage':'denied','region':['TR']});gtag('consent','default',{'ad_storage':'granted','ad_user_data':'granted','ad_personalization':'granted','analytics_storage':'granted'});}
+if(sc){gtag('consent','default',{'ad_storage':sc.ad_storage||'denied','ad_user_data':sc.ad_user_data||'denied','ad_personalization':sc.ad_personalization||'denied','analytics_storage':sc.analytics_storage||'denied','wait_for_update':500});}
+else{gtag('consent','default',{'ad_storage':'denied','ad_user_data':'denied','ad_personalization':'denied','analytics_storage':'denied','wait_for_update':500,'region':['AT','BE','BG','HR','CY','CZ','DK','EE','FI','FR','DE','GR','HU','IE','IT','LV','LT','LU','MT','NL','PL','PT','RO','SK','SI','ES','SE','IS','LI','NO','GB','CH']});gtag('consent','default',{'ad_storage':'denied','ad_user_data':'denied','ad_personalization':'denied','analytics_storage':'granted','wait_for_update':500,'region':['US-CA']});gtag('consent','default',{'ad_storage':'denied','ad_user_data':'denied','ad_personalization':'denied','analytics_storage':'denied','wait_for_update':500,'region':['BR']});gtag('consent','default',{'ad_storage':'granted','ad_user_data':'denied','ad_personalization':'denied','analytics_storage':'granted','wait_for_update':500,'region':['JP','KR']});gtag('consent','default',{'ad_storage':'denied','ad_user_data':'denied','ad_personalization':'denied','analytics_storage':'denied','wait_for_update':500,'region':['TR']});gtag('consent','default',{'ad_storage':'granted','ad_user_data':'granted','ad_personalization':'granted','analytics_storage':'granted'});}
 gtag('set','url_passthrough',true);gtag('set','ads_data_redaction',true);
 gtag('js',new Date());gtag('config','AW-1012572629');
+var gs=document.createElement('script');gs.async=true;gs.src='https://www.googletagmanager.com/gtag/js?id=AW-1012572629';document.head.appendChild(gs);
 `,
         }}
       />
-      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-1012572629" />
 
       {/* ── Dark mode (before paint) ── */}
       <script
